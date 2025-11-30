3969f1b973b386619cb13fa95098c9d935feeb75
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Pro-3.local>
2025-11-30 17:00:37 -0500

    register page almost complete, styling needs to be completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+// import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Register from './pages/Register.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+
+</BrowserRouter>
 )
