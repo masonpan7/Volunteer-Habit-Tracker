5890923f6edf5531432b61b7b79463b9936d39b7
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Pro-3.local>
2025-12-01 20:37:16 -0500

    login page

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Register from './pages/Register.tsx'
+import Login from './pages/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
 
 </BrowserRouter>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+// import { useNavigate } from "react-router";
+import "./Register.css"; 
+
+export default function Login() {
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const [msg, setMsg] = useState("");
+//   const navigate = useNavigate();
+
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+        const res = await fetch("http://localhost:8000/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ user, pass }),
+        });
+    
+        const data = await res.json();
+        if (res.ok) {
+            // store username into localStorage
+            localStorage.setItem("user", user);
+
+            setMsg(`Logged in as ${user}`);
+        } else {
+            setMsg(`Error: ${data.detail}`);
+        }
+    } catch (err) {
+        console.error(err);
+        setMsg("Error: Problem connecting to server");
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin} style={{ marginTop: "1rem" }}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          style={{ margin: "0.5rem", padding: "0.5rem" }}
+          required
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          style={{ margin: "0.5rem", padding: "0.5rem" }}
+          required
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+
+      {msg && <p style={{ marginTop: "1rem" }}>{msg}</p>}
+    </div>
+  );
+}
