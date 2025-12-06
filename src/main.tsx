feea5822b9914934b6309385200719df2d61bb46
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Pro-3.local>
2025-12-06 16:43:33 -0500

    fixed up register styling, added navbar (styling needs to be fixed)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 // import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -8,6 +8,12 @@ import Login from './pages/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <nav className="nav-bar">
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/register" element={<Register />} />
