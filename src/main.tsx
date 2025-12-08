ec0173473996d7c41ca3b2c728800cef9a3bf742
masonpan7 <masonpan7@gmail.com>
2025-12-07 21:01:43 -0500

    Updated App.tsx and Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import App from './App.tsx'
 import Register from './pages/Register.tsx'
 import Login from './pages/Login.tsx'
+import LeaderboardPage from './pages/Leaderboard.tsx'
+import TrackerPage from './pages/Tracker_Dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -12,12 +14,16 @@ createRoot(document.getElementById('root')!).render(
       <Link to="/">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+      <Link to = "/tracker">Tracker</Link>
     </nav>
 
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/tracker" element={<TrackerPage />} />
+      <Route path="/leaderboard" element={<LeaderboardPage />} />
     </Routes>
 
 </BrowserRouter>
